42a8bc62845e23b1596609c7b61c49ecba18ebb8
Christian Kellner <christian@kellner.me>
2014-11-18 14:33:01 -0500

    Initial import

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include "mex.h"
+
+// main entry point
+void mexFunction(int            nlhs,
+                 mxArray       *lhs[],
+                 int            nrhs,
+                 const mxArray *rhs[])
+{
+
+}
+
