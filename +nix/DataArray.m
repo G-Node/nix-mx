613261f58826c18fa407615b39659cbb31eb36ad
Christian Kellner <christian@kellner.me>
2014-11-26 17:35:03 -0500

    add info property to DataArray

--- +nix/DataArray.m
+++ +nix/DataArray.m
@@ -4,9 +4,16 @@ classdef DataArray < nix.Entity
     properties
     end
     
+    properties (Dependent)
+      info
+    end
+   
     methods
         function obj = DataArray(h)
            obj@nix.Entity(h);
+        end
+        function nfo = get.info(obj)
+            nfo = nix_mx('DataArray::describe', obj.nix_handle);
         end
         
         function data = read_all_double(obj)
