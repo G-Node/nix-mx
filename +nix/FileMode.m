5bc6ec9007279d42418e44a19b1949472df63374
Christian Kellner <christian@kellner.me>
2015-02-11 14:19:37 +0100

    [+] FileMode: add open mode constants

---
+++ +nix/FileMode.m
@@ -0,0 +1,11 @@
+classdef FileMode
+    %FILEMODE nix file open modes
+    
+    properties (Constant)
+        ReadOnly = uint8(0)
+        ReadWrite = uint8(1)
+        Overwrite = uint8(2)
+    end
+    
+end
+
