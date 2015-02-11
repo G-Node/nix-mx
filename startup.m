934d1e8b337b75903af857c79b176bc51e1390f8
Christian Kellner <christian@kellner.me>
2015-02-11 14:33:37 +0100

    startup: add build/Debug to path if exists

--- startup.m
+++ startup.m
@@ -1,2 +1,7 @@
-addpath(fullfile(pwd, 'build'));
+buildpath = fullfile(pwd, 'build');
+addpath(buildpath);
+dbgpath = fullfile(buildpath, 'Debug');
+if exist(dbgpath, 'file')
+    addpath(dbgpath);
+end
 addpath(pwd);
