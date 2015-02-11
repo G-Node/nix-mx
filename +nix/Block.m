6219090fc31b1e0c6ef21571fb32f75337d75f85
Christian Kellner <christian@kellner.me>
2015-02-11 16:37:50 +0100

    [+] Block: add name, type properties

--- +nix/Block.m
+++ +nix/Block.m
@@ -1,12 +1,27 @@
 classdef Block < nix.Entity
     %Block nix Block object
     
-    properties
+    properties(Hidden)
+        info
+    end
+    
+    properties(Dependent)
+        name
+        type
     end
     
     methods
         function obj = Block(h)
            obj@nix.Entity(h);
+           obj.info = nix_mx('Block::describe', obj.nix_handle);
+        end
+        
+        function name = get.name(block)
+           name = block.info.name;
+        end
+        
+        function type = get.type(block)
+            type = block.info.type;
         end
         
         function das = data_arrays(obj)
