3029e6a3559b91a1154f4e6ef657c1fb6b859f93
Christian Kellner <christian@kellner.me>
2015-02-19 17:06:43 +0100

    make_mx_array for nix::Value

--- src/utils/mkarray.cc
+++ src/utils/mkarray.cc
@@ -12,3 +12,24 @@ mxArray* make_mx_array(const nix::NDSize &size)
 
 	return res;
 }
+
+mxArray* make_mx_array(const nix::Value &v)
+{
+	mxArray *res;
+	nix::DataType dtype = v.type();
+
+	switch (dtype) {
+		case nix::DataType::Bool: res = make_mx_array(v.get<bool>()); break;
+		case nix::DataType::String: res = make_mx_array(v.get<std::string>()); break;
+		case nix::DataType::Double: res = make_mx_array(v.get<double>()); break;
+		case nix::DataType::Int32: res = make_mx_array(v.get<std::int32_t>()); break;
+		case nix::DataType::UInt32: res = make_mx_array(v.get<std::uint32_t>()); break;
+		case nix::DataType::Int64: res = make_mx_array(v.get<std::int64_t>()); break;
+		case nix::DataType::UInt64: res = make_mx_array(v.get<std::uint64_t>()); break;
+
+		default: res = make_mx_array(v.get<std::string>());
+	}
+
+	return res;
+
+}
