f58819c1ee38ce39f1cc7fda9be4e0f57a0eadf2
mpsonntag <michael.p.sonntag@gmail.com>
2015-03-12 16:19:23 +0100

    LinkType make_mx_array

--- src/nixfeature.cc
+++ src/nixfeature.cc
@@ -12,19 +12,9 @@ namespace nixfeature {
 
     mxArray *describe(const nix::Feature &feat)
     {
-        nix::LinkType link_type = feat.linkType();
-        uint8_t ltype;
-
-        switch (link_type) {
-        case nix::LinkType::Tagged: ltype = 0; break;
-        case nix::LinkType::Untagged: ltype = 1; break;
-        case nix::LinkType::Indexed: ltype = 2; break;
-        default: throw std::invalid_argument("unkown link type");
-        }
-
         struct_builder sb({ 1 }, { "id", "linkType" });
         sb.set(feat.id());
-        sb.set(ltype);
+        sb.set(feat.linkType());
         return sb.array();
     }
 
