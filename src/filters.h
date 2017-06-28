14b87f953da78be86ec4ff9bb3551123dfed8afd
mpsonntag <michael.p.sonntag@gmail.com>
2017-06-28 15:13:52 +0200

    [c++] Add find function filter templates

--- src/filters.h
+++ src/filters.h
@@ -13,6 +13,7 @@
 #include "mex.h"
 #include "datatypes.h"
 
+// filter function templates
 template<typename T, typename FN>
 std::vector<T> filterFullEntity(const extractor &input, FN ff) {
     std::vector<T> res;
@@ -111,6 +112,69 @@ std::vector<T> filterEntity(const extractor &input, FN ff) {
     case switchFilter::Ids:
         // this will crash matlab, if its not a vector of strings...
         res = ff(nix::util::IdsFilter<T>(input.vec<std::string>(3)));
+        break;
+    default: throw std::invalid_argument("unknown or unsupported filter");
+    }
+
+    return res;
+}
+
+// find filter function templates
+template<typename T, typename FN>
+std::vector<T> filterFullEntity(const extractor &input, 
+                    uint8_t filter_id, size_t val_pos, size_t max_depth, FN ff) {
+    std::vector<T> res;
+
+    switch (filter_id) {
+    case switchFilter::AcceptAll:
+        res = ff(nix::util::AcceptAll<T>(), max_depth);
+        break;
+    case switchFilter::Id:
+        res = ff(nix::util::IdFilter<T>(input.str(val_pos)), max_depth);
+        break;
+    case switchFilter::Ids:
+        // this will crash matlab, if its not a vector of strings...
+        res = ff(nix::util::IdsFilter<T>(input.vec<std::string>(val_pos)), max_depth);
+        break;
+    case switchFilter::Type:
+        res = ff(nix::util::TypeFilter<T>(input.str(val_pos)), max_depth);
+        break;
+    case switchFilter::Name:
+        res = ff(nix::util::NameFilter<T>(input.str(val_pos)), max_depth);
+        break;
+    case switchFilter::Metadata:
+        res = ff(nix::util::MetadataFilter<T>(input.str(val_pos)), max_depth);
+        break;
+    case switchFilter::Source:
+        res = ff(nix::util::SourceFilter<T>(input.str(val_pos)), max_depth);
+        break;
+    default: throw std::invalid_argument("unknown or unsupported filter");
+    }
+
+    return res;
+}
+
+template<typename T, typename FN>
+std::vector<T> filterNameTypeEntity(const extractor &input,
+                        uint8_t filter_id, size_t val_pos, size_t max_depth, FN ff) {
+    std::vector<T> res;
+
+    switch (filter_id) {
+    case switchFilter::AcceptAll:
+        res = ff(nix::util::AcceptAll<T>(), max_depth);
+        break;
+    case switchFilter::Id:
+        res = ff(nix::util::IdFilter<T>(input.str(val_pos)), max_depth);
+        break;
+    case switchFilter::Ids:
+        // this will crash matlab, if its not a vector of strings...
+        res = ff(nix::util::IdsFilter<T>(input.vec<std::string>(val_pos)), max_depth);
+        break;
+    case switchFilter::Type:
+        res = ff(nix::util::TypeFilter<T>(input.str(val_pos)), max_depth);
+        break;
+    case switchFilter::Name:
+        res = ff(nix::util::NameFilter<T>(input.str(val_pos)), max_depth);
         break;
     default: throw std::invalid_argument("unknown or unsupported filter");
     }
