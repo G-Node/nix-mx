6fb55dbd33763d9bca7bcd37c167cc3cdfe7bd09
mpsonntag <michael.p.sonntag@gmail.com>
2017-06-29 14:56:21 +0200

    [win_build.bat] Update windows build batch file
    Closes #120
    
    Refactors the windows nix-mx build batch file
    - reduces dependencies on secondary batch files.
    - adds comments.
    - copies the auto-generated nixversion.hpp file
    to the required folder.

--- win_build.bat
+++ win_build.bat
@@ -1,15 +1,47 @@
+@ECHO off
+REM Latest dependencies at https://projects.g-node.org/nix/
 SET NIX_DEP=c:\work\nix-dep
+REM clone nix source from https://github.com/G-Node/nix
 SET NIX_ROOT=c:\work\nix
 SET NIX_MX_ROOT=c:\work\nix-mx
-REM Use only build types "Release" or "Debug"
-SET BUILD_TYPE=Release
 
-IF NOT %BUILD_TYPE% == Release (IF NOT %BUILD_TYPE% == Debug (ECHO Please use only Release or Debug as BUILD_TYPE))
+ECHO Use only build types "Release" or "Debug"
+IF "%1" == "Debug" (SET BUILD_TYPE=Debug)
+IF "%BUILD_TYPE%" == "" (SET BUILD_TYPE=Release)
+
+IF NOT %BUILD_TYPE% == Release (IF NOT %BUILD_TYPE% == Debug (ECHO Only Release or Debug are supported build types))
 IF NOT %BUILD_TYPE% == Release (IF NOT %BUILD_TYPE% == Debug (EXIT /b))
-REM Set NIX_BUILD_DIR for nix-mx FindNIX.cmake
-SET NIX_BUILD_DIR=%NIX_ROOT%\build\%BUILD_TYPE%
 
-IF %BUILD_TYPE% == Debug (CALL %NIX_DEP%\nixenv.bat Debug) ELSE (CALL %NIX_DEP%\nixenv.bat)
+ECHO --------------------------------------------------------------------------
+ECHO Setting up environment ...
+ECHO --------------------------------------------------------------------------
+
+IF "%PLATFORM%" == "" (IF %PROCESSOR_ARCHITECTURE% == x86 (SET PLATFORM=x86) ELSE (SET PLATFORM=x64))
+ECHO Platform: %PLATFORM% (%BUILD_TYPE%)
+
+SET BASE=%NIX_DEP%\%PLATFORM%\%BUILD_TYPE%
+
+SET CPPUNIT_INCLUDE_DIR=%BASE%\cppunit-1.13.2\include
+SET PATH=%PATH%;%CPPUNIT_INCLUDE_DIR%
+
+SET HDF5_BASE=%NIX_DEP%\%PLATFORM%\hdf5-1.8.14
+SET HDF5_DIR=%HDF5_BASE%\cmake\hdf5
+SET PATH=%PATH%;%HDF5_BASE%\bin
+
+SET BOOST_ROOT=%BASE%\boost-1.57.0
+SET BOOST_INCLUDEDIR=%BOOST_ROOT%\include\boost-1_57
+
+ECHO CPPUNIT_INCLUDE_DIR=%CPPUNIT_INCLUDE_DIR%
+IF EXIST %CPPUNIT_INCLUDE_DIR% (ECHO cppunit OK) ElSE (EXIT /b)
+ECHO HDF5_DIR=%HDF5_DIR%
+IF EXIST %HDF5_DIR% (ECHO hdf5 OK) ELSE (EXIT /b)
+ECHO BOOST_INCLUDEDIR=%BOOST_INCLUDEDIR%
+IF EXIST %BOOST_ROOT% (ECHO boost OK) ELSE (EXIT /b)
+
+ECHO --------------------------------------------------------------------------
+ECHO Setting up nix build ...
+ECHO --------------------------------------------------------------------------
+SET NIX_BUILD_DIR=%NIX_ROOT%\build\%BUILD_TYPE%
 
 IF NOT EXIST %NIX_ROOT%\build (MKDIR %NIX_ROOT%\build)
 CD %NIX_ROOT%\build
@@ -19,30 +51,55 @@ RD /S /Q "CMakeFiles" "Testing" "Debug" "Release" "nix-tool.dir" "x64" "TestRunn
 
 IF %PROCESSOR_ARCHITECTURE% == x86 ( cmake .. -G "Visual Studio 12") ELSE (cmake .. -G "Visual Studio 12 Win64")
 
-REM Start %NIX_ROOT%\build\nix.sln
-cmake --build . --config %CONFIGURATION% --target nix
+ECHO --------------------------------------------------------------------------
+ECHO Building nix via %NIX_ROOT%\build\nix.sln ...
+ECHO --------------------------------------------------------------------------
+cmake --build . --config %BUILD_TYPE% --target nix
 
+IF %ERRORLEVEL% == 1 (EXIT /b)
+
+ECHO --------------------------------------------------------------------------
+ECHO Testing nix ...
+ECHO --------------------------------------------------------------------------
 %NIX_BUILD_DIR%\TestRunner.exe
 
+REM nix-mx requires nixversion file in ../nix/include/nix
+IF EXIST %NIX_ROOT%\build\include\nix\nixversion.hpp (
+	COPY %NIX_ROOT%\build\include\nix\nixversion.hpp %NIX_ROOT%\include\nix\
+)
+
+ECHO --------------------------------------------------------------------------
+ECHO Setting up nix-mx build ...
+ECHO --------------------------------------------------------------------------
 IF NOT EXIST %NIX_MX_ROOT%\build (MKDIR %NIX_MX_ROOT%\build)
 CD %NIX_MX_ROOT%\build
 REM Clean up build folder to ensure clean build.
 DEL * /S /Q
 RD /S /Q "CMakeFiles" "Debug" "nix_mx.dir" "Release" "Win32" "x64"
 
-COPY %NIX_BUILD_DIR%\nix.dll %NIX_MX_ROOT%\build\ /Y
-COPY %HDF5_BASE%\bin\hdf5.dll %NIX_MX_ROOT%\build\ /Y
-COPY %HDF5_BASE%\bin\msvcp120.dll %NIX_MX_ROOT%\build\ /Y
-COPY %HDF5_BASE%\bin\msvcr120.dll %NIX_MX_ROOT%\build\ /Y
-COPY %HDF5_BASE%\bin\szip.dll %NIX_MX_ROOT%\build\ /Y
-COPY %HDF5_BASE%\bin\zlib.dll %NIX_MX_ROOT%\build\ /Y
+REM Copying required libraries to nix-mx root folder
+COPY %NIX_BUILD_DIR%\nix.dll %NIX_MX_ROOT%\ /Y
+COPY %HDF5_BASE%\bin\hdf5.dll %NIX_MX_ROOT%\ /Y
+COPY %HDF5_BASE%\bin\msvcp120.dll %NIX_MX_ROOT%\ /Y
+COPY %HDF5_BASE%\bin\msvcr120.dll %NIX_MX_ROOT%\ /Y
+COPY %HDF5_BASE%\bin\szip.dll %NIX_MX_ROOT%\ /Y
+COPY %HDF5_BASE%\bin\zlib.dll %NIX_MX_ROOT%\ /Y
 
 IF %PROCESSOR_ARCHITECTURE% == x86 (cmake .. -G "Visual Studio 12") ELSE (cmake .. -G "Visual Studio 12 Win64")
 
-cmake --build . --config %CONFIGURATION%
+ECHO --------------------------------------------------------------------------
+ECHO Building nix-mx via %NIX_MX_ROOT%\build\nix-mx.sln ...
+ECHO --------------------------------------------------------------------------
+cmake --build . --config %BUILD_TYPE%
 
-COPY %NIX_MX_ROOT%\build\%BUILD_TYPE%\nix_mx.mexw* %NIX_MX_ROOT%\build\ /Y
+IF %ERRORLEVEL% == 1 (EXIT /b)
+
+REM Copying required nix-mx.mex file to nix-mx root folder
+COPY %NIX_MX_ROOT%\build\%BUILD_TYPE%\nix_mx.mexw* %NIX_MX_ROOT%\ /Y
 
 CD %NIX_MX_ROOT%
 
+ECHO --------------------------------------------------------------------------
+ECHO Starting matlab ...
+ECHO --------------------------------------------------------------------------
 Start %NIX_MX_ROOT%\startup.m
